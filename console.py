b1c294364b44fed0218ddbe6ebf2e20969854348
sherif mostafa <sherifmostafa765@gmail.com>
2023-12-10 12:19:45 +0000

    s

--- console.py
+++ console.py
@@ -3,12 +3,12 @@ import cmd
 import shlex
 from models.__init__ import storage
 from models.base_model import BaseModel
-from models.User import User
-from models.Amenity import Amenity
-from models.Place import Place
-from models.Review import Review
-from models.State import State
-from models.City import City
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class HBNBCommand(cmd.Cmd):
